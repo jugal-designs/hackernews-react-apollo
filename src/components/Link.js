9f306b794955be839f2ff72724481bef756a8bb8
Jugal Baraiya <jugal.designs@gmail.com>
2018-07-27 23:03:27 +0530

    Loading links query implemented

---
+++ src/components/Link.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+import React, { Component } from 'react'
+
+class Link extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          {this.props.link.description} ({this.props.link.url})
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Link
