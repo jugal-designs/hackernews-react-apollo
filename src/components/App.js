ff5cadca6b810019f5de3a957c2af513d6dc49d7
Administrator <admin@example.com>
2018-07-28 12:49:57 +0530

    test commit

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ export default () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
+      <h1 className="App-title">Welcome to React Demo</h1>
     </header>
     <LinkList />
   </div>
